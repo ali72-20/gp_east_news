e69360e5ef20d153344a48dd92079bcaed2b44ec
ali72-20 <as20201118@gmail.com>
2024-06-18 05:03:30 +0300

    + add news category_package

---
+++ lib/UI/News_category/item_model.dart
@@ -0,0 +1,6 @@
+class item_model{
+  String Category_name;
+  String Category_image;
+
+  item_model({required this.Category_name, required this.Category_image});
+}
---
+++ lib/UI/News_category/items_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class items_list extends StatelessWidget {
+  const items_list({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
