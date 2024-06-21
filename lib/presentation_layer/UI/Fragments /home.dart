9e45369876cdb77b62d71eb91dc3c4c02ebf4872
ali72-20 <as20201118@gmail.com>
2024-06-22 01:01:27 +0300

    % fixed tap bar starting padding

--- lib/presentation_layer/UI/Fragments /home.dart
+++ lib/presentation_layer/UI/Fragments /home.dart
@@ -1,13 +1,10 @@
-
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-
 
 import '../Body/home_top_bar.dart';
 import '../News/news_list.dart';
 import '../Body/tap_item.dart';
 import '../colors/colors.dart';
-
 
 class home extends StatefulWidget {
   const home({super.key});
@@ -22,21 +19,18 @@ class _homeState extends State<home> {
     return DefaultTabController(
       length: 6,
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(-8),
-            child: Align(
-              alignment: Alignment.centerLeft,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(-8),
               child: TabBar(
+                tabAlignment: TabAlignment.center,
                 isScrollable: true,
-                dragStartBehavior: DragStartBehavior.start,
                 dividerColor: Colors.transparent,
                 indicatorColor: primary_color,
                 unselectedLabelColor: Colors.black,
                 indicatorSize: TabBarIndicatorSize.tab,
                 labelColor: primary_color,
-                indicatorPadding: EdgeInsets.zero,
                 tabs: [
                   Tab(
                     child: tap_item(
@@ -54,36 +48,42 @@ class _homeState extends State<home> {
                     ),
                   ),
                   Tab(
-                    child: tap_item(title: "Business",),
+                    child: tap_item(
+                      title: "Business",
+                    ),
                   ),
                   Tab(
-                    child: tap_item(title: "Science",),
+                    child: tap_item(
+                      title: "Science",
+                    ),
                   ),
                   Tab(
-                    child: tap_item(title: "Technology",),
+                    child: tap_item(
+                      title: "Technology",
+                    ),
                   ),
                 ],
               ),
             ),
           ),
-        ),
-        body: TabBarView(children: [
-          CustomScrollView(
-            physics: const BouncingScrollPhysics(),
-            slivers: [
-              const SliverToBoxAdapter(
-                child: home_top_bar(),
+          body: TabBarView(
+            children: [
+              CustomScrollView(
+                physics: const BouncingScrollPhysics(),
+                slivers: [
+                  const SliverToBoxAdapter(
+                    child: home_top_bar(),
+                  ),
+                  news_list(),
+                ],
               ),
-              news_list(),
+              Text("Sports"),
+              Text("Politics"),
+              Text("Busniss"),
+              Text("Science"),
+              Text("Tecnology"),
             ],
-          ),
-           Text("Sports"),
-           Text("Politics"),
-           Text("Busniss"),
-           Text("Science"),
-           Text("Tecnology"),
-        ],)
-      ),
+          )),
     );
   }
 }
