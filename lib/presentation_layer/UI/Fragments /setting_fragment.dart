220a149d05c03a45a674cdb5a816dbb05a8ab901
ali72-20 <as20201118@gmail.com>
2024-06-28 16:37:20 +0300

    Start in settings fragment

--- lib/presentation_layer/UI/Fragments /setting_fragment.dart
+++ lib/presentation_layer/UI/Fragments /setting_fragment.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:gp_east_news/presentation_layer/UI/colors/colors.dart';
+import 'package:ionicons/ionicons.dart';
 
 class setting_fragment extends StatelessWidget {
   const setting_fragment({super.key});
@@ -7,7 +10,34 @@ class setting_fragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("setting"),
-    );
+        body: Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      child: Column(
+        children: [
+          Text(
+            'Settings',
+            style: TextStyle(color: primary_color, fontFamily: 'Poppins'),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            width: double.infinity,
+            child:  Row(
+              children: [
+              Container(
+                height: 50,
+                width: 50,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.redAccent
+                ),
+                child:const Icon(Ionicons.earth)
+              ),
+                const SizedBox(width: 8,),
+                const Text('Language', style: TextStyle(color: Colors.redAccent, fontFamily: 'Poppins'),)
+            ],),
+          )
+        ],
+      ),
+    ));
   }
 }
