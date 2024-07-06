a5e605ef9965449faf89c6a17ebc9d73c74d9cfd
ali72-20 <as20201118@gmail.com>
2024-07-06 21:40:47 +0300

    bug solved

--- lib/Feature/comments /presentatio_layer/view/widgets/commentsListView.dart
+++ lib/Feature/comments /presentatio_layer/view/widgets/commentsListView.dart
@@ -1,10 +1,12 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
 import 'package:gp_east_news/Core/Assets/assets_data.dart';
 import 'package:gp_east_news/Core/Messages/toast_message.dart';
 import 'package:gp_east_news/Feature/Main/Presentation_layer/views/mainScreen.dart';
-import 'package:gp_east_news/Feature/News/presentation_layer/views/widgets/newe_content_view.dart';
 import 'package:gp_east_news/Feature/comments%20/data_layer/Api/deletComment.dart';
 import 'package:gp_east_news/Feature/comments%20/data_layer/Api/editComment.dart';
 import 'package:gp_east_news/Feature/comments%20/presentatio_layer/view/widgets/comment_item_view.dart';
@@ -19,7 +21,6 @@ class commentListView extends StatefulWidget {
   State<commentListView> createState() => _commentListViewState();
 }
 
-
 class _commentListViewState extends State<commentListView> {
   TextEditingController newCommentController = TextEditingController();
 
@@ -28,95 +29,105 @@ class _commentListViewState extends State<commentListView> {
     return ListView.builder(
       itemCount: widget.list.length,
       itemBuilder: (context, index) {
-        return user_model.id == widget.list[index].authorId ?
-        Dismissible(
-          onDismissed: (direction) {
-            if (direction == DismissDirection.endToStart) {
-              setState(() {
-                widget.list.removeAt(index);
-              });
-            } else {
-              setState(() {
-                widget.list[index].text = newCommentController.text;
-              });
-            }
-          },
-          confirmDismiss: (DismissDirection direction) {
-            if (direction == DismissDirection.endToStart) {
-              return showDialog(
-                  context: context, builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text('Delete comment',
-                    style: TextStyle(fontFamily: kPrimaryFont),),
-                  content: const Text('You need to delete comment!!'),
-                  actions: [
-                    TextButton(onPressed: () async {
-                      bool result = await DeleteComment(Dio()).delete(
-                          authorId: user_model.id,
-                          commentId: widget.list[index].id);
-                      if (result) {
-                        ToastMessage().showMessage(message: 'Comment deleted');
-                        Navigator.of(context).pop((true));
-                      }
-                    }, child: const Text('Delete'),)
-                  ],
-                );
-              });
-            } else {
-              return showDialog(
-                  context: context, builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text('Edit comment',
-                    style: TextStyle(fontFamily: kPrimaryFont),),
-                  content: const Text('You need to Edite comment!!'),
-                  actions: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextField(
-                            controller: newCommentController,
-                            decoration: InputDecoration(
-                              hintText: 'Add a comment'.tr,
-                              border: InputBorder.none,
-                            ),
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () async {
-                            if (newCommentController.text.isNotEmpty) {
-                              bool result = await EditComment(Dio()).edit(text: newCommentController.text, commentId: widget.list[index].id);
-                              if (result) {
-                                ToastMessage().showMessage(message: 'Comment Edit');
-                                Navigator.of(context).pop((true));
-                              }
-                            }
-                            Focus.of(context).unfocus();
-                          },
-                          icon: const Icon(Icons.send),
-                        ),
-                      ],
-                    ),
-                  ],
-                );
-              });
-            }
-          },
-          key: ValueKey<commentModel>(widget.list[index]),
-          child: commentItemView(model: widget.list[index],),
-          background: Container(
-            color: Colors.blueAccent,
-            child: const Icon(Icons.edit, color: Colors.white,),
-            alignment: Alignment.centerLeft,
+        return user_model.id == widget.list[index].authorId
+            ? Slidable(
+          child: commentItemView(
+            model: widget.list[index],
           ),
-          secondaryBackground: Container(
-              color: Colors.red,
-              alignment: Alignment.centerRight,
-              child: const Icon(Icons.delete, color: Colors.white,)
+          startActionPane: ActionPane(
+            motion: StretchMotion(),
+            children: [
+              SlidableAction(
+                icon: Icons.edit,
+                label: 'Edit',
+                backgroundColor: Colors.blue,
+                onPressed: (context) async {
+                  await edit(
+                      context, widget.list[index].id, widget.list[index].text, index);
+                },
+              ),
+            ],
+          ),
+          endActionPane: ActionPane(
+            motion: StretchMotion(),
+            children: [
+              SlidableAction(
+                icon: Icons.delete,
+                label: 'Delete',
+                backgroundColor: Colors.red,
+                onPressed: (context) async {
+                  bool result = await delete(context, widget.list[index].id,
+                      widget.list[index].authorId);
+                  log("controller: ${newCommentController.text}");
+                  if (result) {
+                    setState(() {
+                      widget.list.removeAt(index);
+                    });
+                    ToastMessage().showMessage(message: "Comment deleted");
+                  }
+                },
+              ),
+            ],
           ),
         )
             : SingleChildScrollView(
-            child: commentItemView(model: widget.list[index],));
+            child: commentItemView(
+              model: widget.list[index],
+            ));
       },
+    );
+  }
+
+  Future<bool> delete(BuildContext context, String commentID,
+      String authorID) async {
+    bool res = await DeleteComment(Dio()).delete(
+        authorId: authorID, commentId: commentID);
+    return res;
+  }
+
+  Future<void> edit(BuildContext context, String commentID, String text, int index) async {
+    newCommentController.text = text;
+    bool res = false;
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Edit comment',
+              style: TextStyle(fontFamily: kPrimaryFont),),
+            actions: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: newCommentController,
+                      decoration: InputDecoration(
+                        hintText: 'Add a comment'.tr,
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () async {
+                      Navigator.pop(context);
+                      if (newCommentController.text.isNotEmpty) {
+                        res = await EditComment(Dio()).edit(
+                            text: newCommentController.text,
+                            commentId: commentID);
+                        if(res){
+                          setState(() {
+                            widget.list[index].text = newCommentController.text;
+                          });
+                          ToastMessage().showMessage(message: 'Comment Edit');
+                        }
+                      }
+                    },
+                    icon: const Icon(Icons.send),
+                  ),
+                ],
+              ),
+            ],
+          );
+        }
     );
   }
 }
