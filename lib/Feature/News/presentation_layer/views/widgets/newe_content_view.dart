6720389cbe019220e910bdd59f2eeab530f01cc2
ali72-20 <as20201118@gmail.com>
2024-07-06 23:05:40 +0300

    launcher url done

--- lib/Feature/News/presentation_layer/views/widgets/newe_content_view.dart
+++ lib/Feature/News/presentation_layer/views/widgets/newe_content_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gp_east_news/Core/Assets/assets_data.dart';
+import 'package:gp_east_news/Core/Messages/toast_message.dart';
 import 'package:gp_east_news/Feature/News/Data_layer/Api/news_servieces/news_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class newsContentView extends StatelessWidget {
    newsContentView({super.key, required this.item});
@@ -24,8 +26,16 @@ class newsContentView extends StatelessWidget {
           physics: const BouncingScrollPhysics(),
           child: Container(
             margin: const EdgeInsets.all(12),
-            child: Text(
-              item.content ?? item.url!,
+            child: item.content == null? TextButton(
+              child: Text(item.url!, style: const TextStyle(color: Colors.blue),
+               ),
+              onPressed: ()async{
+                await openArticleUrl();
+              },
+            ) :
+
+            Text(
+              item.content!,
               style: const TextStyle(
                   color: Colors.black, fontFamily: kPrimaryFont),
             ),
@@ -33,5 +43,14 @@ class newsContentView extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> openArticleUrl() async {
+     final url = Uri.parse(item.url!);
+    if(await canLaunchUrl(url)){
+      await(launchUrl(url));
+    }else{
+      ToastMessage().showMessage(message: 'Some thing wrong');
+    }
   }
 }
