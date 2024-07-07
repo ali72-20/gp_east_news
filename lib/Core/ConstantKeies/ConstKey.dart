29f4a4f87de40a018f00bf99f1b498f29417279d
ali72-20 <as20201118@gmail.com>
2024-07-07 09:33:21 +0300

    add const key

--- lib/Core/ConstantKeies/ConstKey.dart
+++ lib/Core/ConstantKeies/ConstKey.dart
@@ -1,4 +1,9 @@
 class constKey{
   static const String onBoaring = 'onboarding';
   static const String ip = '192.168.164.236'; //192.168.164.236
+  static const String token = 'token'; //192.168.164.236
+  static const String id = 'id'; //192.168.164.236
+  static const String mail = 'mail'; //192.168.164.236
+  static const String userName = 'userName'; //192.168.164.236
+  static const String password = 'password'; //192.168.164.236
 }
